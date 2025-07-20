a90dc9894855abb00dbfc1ed7291fe50bce0f181
Tim Barnes <tw@timandmarina.com>
2025-07-19 22:14:31 -0700

    Add list accessor functions to s1-core.scm
    - Add common car/cdr compositions: cadr, cdar, caar, cddr, etc.
    - Include 2-level, 3-level, and 4-level accessors
    - All functions defined using existing car and cdr primitives
    - Organized by complexity level with clear comments
    - Functions work correctly in REPL testing
    - Provides convenient access to nested list elements
    - Essential for list processing and data structure manipulation

--- scheme/s1-core.scm
+++ scheme/s1-core.scm
@@ -11,4 +11,35 @@
 ;; (define (primitive? x) (eq? (type-of x) 'primitive))
 ;; (define (env-frame? x) (eq? (type-of x) 'env-frame))
 ;; (define (nil? x) (eq? (type-of x) 'nil)) 
+
+;; List accessor functions (compositions of car and cdr)
+;; These provide convenient access to nested list elements
+
+;; Basic 2-level accessors
+(define cadr (lambda (l) (car (cdr l))))      ;; Second element of a list
+(define cdar (lambda (l) (cdr (car l))))      ;; Cdr of the first element
+(define caar (lambda (l) (car (car l))))      ;; Car of the first element
+(define cddr (lambda (l) (cdr (cdr l))))      ;; Cdr of the cdr of a list
+
+;; 3-level accessors
+(define caddr (lambda (l) (car (cdr (cdr l)))))     ;; Third element of a list
+(define cadddr (lambda (l) (car (cdr (cdr (cdr l)))))) ;; Fourth element of a list
+(define cadar (lambda (l) (car (cdr (car l)))))     ;; Car of cdr of car
+(define cddar (lambda (l) (cdr (cdr (car l)))))     ;; Cdr of cdr of car
+(define caadr (lambda (l) (car (car (cdr l)))))     ;; Car of car of cdr
+(define cdadr (lambda (l) (cdr (car (cdr l)))))     ;; Cdr of car of cdr
+(define cdddr (lambda (l) (cdr (cdr (cdr l)))))     ;; Cdr of cdr of cdr
+
+;; 4-level accessors (less common but useful)
+(define caaar (lambda (l) (car (car (car l)))))     ;; Car of car of car
+(define cdaar (lambda (l) (cdr (car (car l)))))     ;; Cdr of car of car
+(define caaadr (lambda (l) (car (car (car (cdr l)))))) ;; Car of car of car of cdr
+(define cdaadr (lambda (l) (cdr (car (car (cdr l)))))) ;; Cdr of car of car of cdr
+(define cadadr (lambda (l) (car (cdr (car (cdr l)))))) ;; Car of cdr of car of cdr
+(define cddadr (lambda (l) (cdr (cdr (car (cdr l)))))) ;; Cdr of cdr of car of cdr
+(define caaddr (lambda (l) (car (car (cdr (cdr l)))))) ;; Car of car of cdr of cdr
+(define cdaddr (lambda (l) (cdr (car (cdr (cdr l)))))) ;; Cdr of car of cdr of cdr
+(define cdddr (lambda (l) (cdr (cdr (cdr l)))))      ;; Cdr of cdr of cdr
+(define cddddr (lambda (l) (cdr (cdr (cdr (cdr l)))))) ;; Cdr of cdr of cdr of cdr
+
 (display "loaded ok\n")
