1f2198790969af294f5d4b872eab05599a3849cb
Tim Barnes <tw@timandmarina.com>
2025-09-04 17:03:50 -0700

    Removed obsolete function load_builtin

--- src/builtin/mod.rs
+++ src/builtin/mod.rs
@@ -69,38 +69,6 @@ fn help(ec: &mut EvalContext, args: &[GcRef]) -> Result<GcRef, String> {
     }
 }
 
-/// Builtin function: (load filename)
-///
-/// Loads and evaluates a Scheme file.
-// pub fn load_builtin(heap: &mut GcHeap, args: &[GcRef]) -> Result<GcRef, String> {
-//     if args.len() != 1 {
-//         return Err("load: expected exactly 1 argument".to_string());
-//     }
-//     let filename = match &args[0].value {
-//         SchemeValue::Str(filename) => filename.clone(),
-//         _ => return Err("load: argument must be a string".to_string()),
-//     };
-//     // Read the file content
-//     let content = match std::fs::read_to_string(&filename) {
-//         Ok(content) => content,
-//         Err(e) => return Err(format!("load: could not read file '{}': {}", filename, e)),
-//     };
-//     // Create a new port for the file
-//     let file_port = crate::gc::new_port_simple(heap, crate::io::PortKind::StringPortInput {
-//         content,
-//         pos: 0,
-//     });
-//     // Get the symbol for **port-stack**
-//     let port_stack_sym = heap.intern_symbol("**port-stack**");
-//     // Get the current port stack from the environment
-//     let env = heap.get_env_mut(); // This assumes you have a way to get the current environment from the heap or context
-//     let current_stack = env.get_symbol(port_stack_sym).unwrap_or_else(|| heap.nil_simple());
-//     // Push the new port onto the stack
-//     let new_stack = crate::gc::new_pair_simple(heap, file_port, current_stack);
-//     env.set_symbol(port_stack_sym, new_stack);
-//     Ok(new_string_simple(heap, &format!("Loaded file: {}", filename)))
-// }
-
 /// Register all builtin functions in the environment
 pub fn register_builtins(heap: &mut GcHeap, env: &mut Environment) {
     debug::register_debug_builtins(heap, env);
